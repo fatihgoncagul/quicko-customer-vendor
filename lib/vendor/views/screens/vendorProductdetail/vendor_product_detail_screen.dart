66fdaef2bca4e5ec72befec84687e948ac09a4e1
Berkay Kaptan <kaptanberkay35@hotmail.com>
2023-06-21 01:51:07 +0300

    UI Push v7

--- lib/vendor/views/screens/vendorProductdetail/vendor_product_detail_screen.dart
+++ lib/vendor/views/screens/vendorProductdetail/vendor_product_detail_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:quicko/vendor/views/screens/edit_product_screen.dart';
 
 import '../../../../utils/show_snackbar.dart';
 
