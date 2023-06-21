bc83460f2c086a0af06bf93d9ff8d3e10aebb7d7
Fatih Goncagül <fthgnc48@icloud.com>
2023-06-21 04:17:33 +0300

    category view updated

--- lib/views/customers/nav_screens/widgets/home_products.dart
+++ lib/views/customers/nav_screens/widgets/home_products.dart
@@ -3,16 +3,25 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:quicko/views/customers/productDetail/product_detail_screen.dart';
 
-class HomeProductWidget extends StatelessWidget {
+class HomeProductWidget extends StatefulWidget {
   final String categoryName;
 
-  const HomeProductWidget({super.key, required this.categoryName});
+  const HomeProductWidget({Key? key, required this.categoryName}) : super(key: key);
+
+  @override
+  _HomeProductWidgetState createState() => _HomeProductWidgetState();
+}
+
+class _HomeProductWidgetState extends State<HomeProductWidget> {
+  final int initialDisplayCount = 2;
+  int displayCount = 2;
 
   @override
   Widget build(BuildContext context) {
     final Stream<QuerySnapshot> _productsStream = FirebaseFirestore.instance
         .collection('products')
-        .where('category', isEqualTo: categoryName).where('approved',isEqualTo: true)
+        .where('category', isEqualTo: widget.categoryName)
+        .where('approved', isEqualTo: true)
         .snapshots();
 
     return StreamBuilder<QuerySnapshot>(
@@ -23,68 +32,118 @@ class HomeProductWidget extends StatelessWidget {
         }
 
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
+          return CircularProgressIndicator();
         }
-        return Container(
-          height: 280,
-          child: ListView.separated(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              final productData = snapshot.data!.docs[index];
-              return GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder:(context){
-                    return ProductDetailScreen(productData: productData,);
-                  }));
-                },
-                child: Card(
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 80,
-                        width: 150,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: NetworkImage(
-                              productData['imageUrlList'][0],
-                            ),
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return Text("No products available");
+        }
+
+        final products = snapshot.data!.docs;
+        final int productCount = products.length;
+        final int _productCount = displayCount.clamp(0, productCount);
+        final bool hasMoreProducts = displayCount < productCount;
+
+        return Column(
+          children: [
+            GridView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12.0,
+                mainAxisSpacing: 12.0,
+                mainAxisExtent: 240,
+              ),
+              itemCount: _productCount,
+              itemBuilder: (context, index) {
+                final productData = products[index];
+                final productName =
+                    '${productData['productName'][0].toUpperCase()}${productData['productName'].substring(1)}';
+
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) {
+                        return ProductDetailScreen(
+                          productData: productData,
+                        );
+                      }),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16.0),
+                      color: Colors.grey.shade200,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(16.0),
+                            topRight: Radius.circular(16.0),
+                          ),
+                          child: Image.network(
+                            "${productData['imageUrlList'][0]}",
+                            height: 170,
+                            width: double.infinity,
                             fit: BoxFit.cover,
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          productData['productName'],
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 4,
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                productName,
+                                style: Theme.of(context).textTheme.subtitle1!.merge(
+                                  const TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 8.0),
+                              Text(
+                                "\$${productData['productPrice'].toStringAsFixed(2)}",
+                                style: Theme.of(context).textTheme.subtitle2!.merge(
+                                  TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey.shade500,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          '\$' + productData['productPrice'].toStringAsFixed(2),
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 4,
-                            color: Colors.yellow.shade900,
-                          ),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+            if (hasMoreProducts)
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      displayCount += 2;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    backgroundColor: Colors.grey.shade100, // Change background color
+                  ),
+                  child: Text(
+                    'View More',
+                    style: TextStyle(color: Colors.blue.shade300, fontSize: 16), // Adjust text color and size
                   ),
                 ),
-              );
-            },
-            separatorBuilder: (context, _) => SizedBox(
-              width: 15,
-            ),
-            itemCount: snapshot.data!.docs.length,
-          ),
+              ),
+          ],
         );
       },
     );
